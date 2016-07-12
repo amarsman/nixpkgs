1b296c5c1989541ebb3c4dca5d9ab8ebc755c072
Aschwin Marsman <a.marsman@aynik.com>
2016-07-12 09:29:34 +0200

    initial version for tlspool-gui

---
+++ pkgs/tools/networking/tlspool-gui/default.nix
@@ -0,0 +1,30 @@
+{ stdenv, fetchurl, qt5, cmake, tlspool }:
+
+stdenv.mkDerivation rec {
+  name = "tlspool-gui-${version}";
+  version = "0.0.1";
+  src = ./../../../../../tlspool-gui/. ;
+
+#  src = fetchgit {
+#    url = "https://github.com/amarsman/tlspool-gui";
+#    rev = "refs/tags/v${version}";
+#    sha256 = "";
+#  };
+
+  buildInputs = [
+    qt5.qtbase qt5.qtsvg qt5.qttranslations cmake tlspool
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/lib $out/sbin $out/man
+    cp tlspool-gui $out/bin
+    '';
+
+  meta = with stdenv.lib; {
+    description = "A Qt based GUI for the TLS pool daemon";
+    homepage = http://tlspool.org/;
+    maintainers = with maintainers; [ amarsman ];
+    platforms = platforms.linux;
+    license = licenses.gpl2;
+  };
+}
